aff21cc5bad1c982046632574f550f55a317a613
zidane-itb <16521420@mahasiswa.itb.ac.id>
2023-04-11 01:54:37 +0700

    commit algorithm and file parser without testing :3

---
+++ src/app/io/filehandler.py
@@ -0,0 +1,48 @@
+from model.node import Node
+
+
+class FileInputHandler:
+
+    @staticmethod
+    def load_file(path: str) -> (list[Node], list[list[float]]):
+        file1 = open(path, 'r')
+
+        lines = file1.readlines()
+        node_count = -1
+        node_list: list[Node] = []
+        adj_matrix: list[list[float]] = [[]]
+        for i, line in enumerate(lines):
+            if i == 0:  # matrix size / node count
+                try:
+                    node_count = int(line)
+                except ValueError:
+                    raise "error while processing file"
+                continue
+
+            if 0 < i < node_count + 1:
+                coordinates = line.split(' ')
+                if len(coordinates) != 2:
+                    raise "error while processing file"
+                try:
+                    node_list.append(Node(float(coordinates[0]), float(coordinates[1])))
+                except ValueError:
+                    raise "error while processing file"
+
+                continue
+
+            if i >= node_count + 1:
+                weights = line.split(' ')
+                if len(weights) != node_count:
+                    raise "error while processing file"
+                try:
+                    adj_matrix.append([0.0 for _ in range(node_count)])
+                    v_idx = i - (node_count + 1)
+                    for j, weight in enumerate(weights):
+                        adj_matrix[v_idx][j] = float(weight)
+
+                except ValueError:
+                    raise "error while processing file"
+
+                continue
+
+        return node_list, adj_matrix
---
+++ src/app/model/node.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+
+class Node:
+
+    def __init__(self, node_id: int, x: float, y: float):
+        # a node must be retrievable from node list by its node id
+        self.node_id = node_id
+        self.x = x
+        self.y = y
+
+    def __eq__(self, compare_object: Node) -> bool:
+        return self.node_id == compare_object.node_id
+
+    def __str__(self):
+        return str(self.node_id)
