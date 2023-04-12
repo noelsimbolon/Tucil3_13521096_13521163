0126b3feca66672453c6a2f8f1abb6951584e503
noelsimbolon <84700640+noelsimbolon@users.noreply.github.com>
2023-04-12 17:25:24 +0700

    feat, style: add function to calculate geodesic distance, reformat code

--- src/common/math.py
+++ src/common/math.py
@@ -1,7 +1,23 @@
-from model.node import Node
-
 import math
 
+from geopy.distance import distance
 
-def euclidean_distance(node_1: Node, node_2: Node) -> float:
-    return math.sqrt(math.pow(node_1.y - node_2.y, 2) + math.pow(node_1.x - node_2.x, 2))
+from src.model.node import Node
+
+
+def euclidean_distance(first_node: Node, second_node: Node) -> float:
+    """
+    :param first_node: the first node
+    :param second_node: the second node
+    :return: the Euclidean distance between the two nodes
+    """
+    return math.sqrt(math.pow(first_node.y - second_node.y, 2) + math.pow(first_node.x - second_node.x, 2))
+
+
+def geodesic_distance(first_coords: tuple[float, float], second_coords: tuple[float, float]) -> float:
+    """
+    :param first_coords: the coordinates of the first location on Earth's surface
+    :param second_coords: the coordinates of the second location on Earth's surface
+    :return: the geodesic distance between the two locations (in meters)
+    """
+    return distance(first_coords, second_coords).meters
